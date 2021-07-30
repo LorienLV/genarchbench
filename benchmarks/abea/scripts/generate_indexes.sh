58b1330f130dae3c693c72ca24b5faf5a57a24f9
Lorien <lorienlopezv@gmail.com>
2021-07-30 18:14:58 +0200

    ABEA: generate index files script.

---
+++ benchmarks/abea/scripts/generate_indexes.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#PJM -N ABEA.CONFIG
+#PJM -L elapse=00:30:00
+#PJM -L node=1
+#PJM --mpi "proc=1,max-proc-per-node=1"
+
+export OMP_NUM_THREADS=1
+
+# The folder that contains the inputs.
+inputs_path="/fefs/scratch/bsc18/bsc18248/genarch-inputs/abea"
+
+scriptfolder="$(realpath $0)"
+binaries_path="$(dirname "$(dirname "$(realpath $0)")")"
+binary="$binaries_path/f5c_gcc" # The ABEA binary to use.
+
+if [[ -z "$inputs_path" || ! -d "$inputs_path" ]]; then
+  echo "ERROR: You have not set a valid input folder $inputs_path"
+  exit 1
+fi
+
+echo "Creating the index files..."
+
+"$binary" index -d "$inputs_path"/fast5_files "$inputs_path"/1000reads.fastq
+"$binary" index -d "$inputs_path"/fast5_files "$inputs_path"/10000reads.fastq
+
+if [ $? -eq 0 ]; then
+    echo "SUCCESS"
+    exit 0
+else
+    echo "ERROR"
+    exit 1
+fi
