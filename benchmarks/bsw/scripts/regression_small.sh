de6a538db913bb4fac1b325bd06922e584584293
Lorien <lorienlopezv@gmail.com>
2021-09-14 13:36:34 +0200

    BSW: regression_test_small sanity check added.

--- benchmarks/bsw/scripts/regression_small.sh
+++ benchmarks/bsw/scripts/regression_small.sh
@@ -2,7 +2,6 @@
 
 # The folder that contains the inputs for this app.
 inputs_path="/fefs/scratch/bsc18/bsc18248/genarch-inputs/bsw/small"
-inputs_path="/home/lorien/Repos/genomicsbench/inputs/genarch-inputs/bsw/small"
 
 if [[ -z "$inputs_path" || ! -d "$inputs_path" ]]; then
   echo "ERROR: You have not set a valid input folder $inputs_path"
@@ -13,7 +12,7 @@ scriptfolder="$(dirname $(realpath $0))"
 binaries_path="$(dirname "$scriptfolder")"
 
 # Clean the stage folder of the jobs after finishing? 1 -> yes, 0 -> no.
-clean=0
+clean=1
 
 # The name of the job.
 job="BSW-REGRESSION-SMALL"
@@ -38,7 +37,7 @@ job_options=(
 #)
 commands=(
     "$binaries_path/build_gcc/main_bsw"
-    # "$binaries_path/build_fcc/main_bsw"
+    "$binaries_path/build_fcc/main_bsw"
 )
 
 # Additional arguments to pass to the commands.
@@ -47,13 +46,13 @@ command_opts="-pairs \"$inputs_path/bandedSWA_SRR7733443_100k_input.txt\" -t \$O
 # Nodes, MPI ranks and OMP theads used to execute with each command.
 parallelism=(
     'nodes=1, mpi=1, omp=1'
-    # 'nodes=1, mpi=1, omp=2'
-    # 'nodes=1, mpi=1, omp=4'
-    # 'nodes=1, mpi=1, omp=8'
-    # 'nodes=1, mpi=1, omp=12'
-    # 'nodes=1, mpi=1, omp=24'
-    # 'nodes=1, mpi=1, omp=36'
-    # 'nodes=1, mpi=1, omp=48'
+    'nodes=1, mpi=1, omp=2'
+    'nodes=1, mpi=1, omp=4'
+    'nodes=1, mpi=1, omp=8'
+    'nodes=1, mpi=1, omp=12'
+    'nodes=1, mpi=1, omp=24'
+    'nodes=1, mpi=1, omp=36'
+    'nodes=1, mpi=1, omp=48'
 )
 
 #
@@ -78,18 +77,15 @@ before_run() (
 after_run() (
     job_name="$1"
 
-    # benchmark_time="$(tac "$job_name.err" | grep -m 1 "Time.Benchmark" | tr -s ' ' | cut -d ' ' -f 3)"
-    # alignment_time="$(tac "$job_name.err" | grep -m 1 "Time.Alignment" | tr -s ' ' | cut -d ' ' -f 4)"
+    pairs_processed="$(cat "$job_name.out" | grep "Total Pairs processed" | cut -d ' ' -f 4)"
+    kernel_time="$(cat "$job_name.out" | grep "Overall SW cycles" | cut -d ' ' -f 6)"
 
-    # echo "Time.Benchmark: $benchmark_time s"
-    # echo "Time.Alignment: $alignment_time s"
+    if [[ $pairs_processed -ne 100000 ]]; then
+        echo "Error in the execution $pairs_processed"
+        return 1 # Failure
+    fi
 
-    # # Check if the output file is identical to the reference
-    # diff --brief "checksum.file" "$inputs_path/output-reference.file" > /dev/null 2>&1
-    # if [[ $? -ne 0 ]]; then
-    #     echo "The output file is not identical to the reference file"
-    #     return 1 # Failure
-    # fi
+    echo "Kernel execution time $kernel_time s"
 
     return 0 # OK
 )
