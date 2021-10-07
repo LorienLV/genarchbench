6d59b7da2c1005315d56f2aeef74a82326db1244
Lorien <lorienlopezv@gmail.com>
2021-10-07 20:47:17 +0200

    Parallel WFA added (not tested)

---
+++ benchmarks/wfa/scripts/compile_ctearm.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#PJM -N CHAIN.COMPILE
+#PJM -L elapse=00:30:00
+#PJM -L node=1
+#PJM --mpi "proc=1,max-proc-per-node=1"
+
+export OMP_NUM_THREADS=1
+
+# The compiler to use, "gcc" and/or "fcc".
+compilers=(
+    'gcc'
+    'fcc'
+)
+
+for compiler in "${compilers[@]}"; do
+    echo "----- Compiling with $compiler -----"
+
+    case "$compiler" in
+        gcc)
+            module load gcc/10.2.0
+            make CC=gcc CXX=g++ FOLDER_BUILD=build_gcc FOLDER_BIN=bin_gcc
+            ;;
+        fcc)
+            module load fuji
+            make CC=fcc CXX=FCC FOLDER_BUILD=build_fcc FOLDER_BIN=bin_fcc
+            ;;
+        *)
+            echo "ERROR: Compiler '$compiler' not supported."
+            exit 1
+    esac
+done
---
+++ benchmarks/wfa/scripts/regression_small.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+
+# The folder that contains the inputs for this app.
+inputs_path="$GENARCH_BENCH_INPUTS_ROOT/wfa/small"
+
+if [[ -z "$inputs_path" || ! -d "$inputs_path" ]]; then
+    echo "ERROR: You have not set a valid input folder $inputs_path"
+    exit 1
+fi
+
+scriptfolder="$(dirname $(realpath $0))"
+binaries_path="$(dirname "$scriptfolder")"
+
+# Clean the stage folder of the jobs after finishing? 1 -> yes, 0 -> no.
+clean=1
+
+# The name of the job.
+job="WFA-REGRESSION-SMALL"
+
+# Commands to run.
+# You can access the number of mpi-ranks using the environment variable
+# MPI_RANKS and the number of omp-threads using the environment variable
+# OMP_NUM_THREADS:
+# commands=(
+#    'command $MPI_RANKS $OMP_NUM_THREADS'
+#)
+# OR
+# commands=(
+#    "command \$MPI_RANKS \$OMP_NUM_THREADS"
+#)
+
+# Nodes, MPI ranks and OMP theads used to execute with each command.
+# parallelism=(
+#     'nodes=1, mpi=1, omp=1'
+#     'nodes=1, mpi=1, omp=2'
+#     'nodes=1, mpi=1, omp=4'
+#     'nodes=1, mpi=1, omp=8'
+#     'nodes=1, mpi=1, omp=12'
+#     'nodes=1, mpi=1, omp=24'
+#     'nodes=1, mpi=1, omp=36'
+#     'nodes=1, mpi=1, omp=48'
+# )
+
+# job additional parameters.
+# job_options=(
+#     # '--exclusive'
+#     # '--time=00:00:01'
+#     # '--qos=debug'
+# )
+
+case "$GENARCH_BENCH_CLUSTER" in
+MN4)
+    commands=(
+        "$binaries_path/bin_gcc/align_benchmark"
+    )
+
+    parallelism=(
+        'nodes=1, mpi=1, omp=1'
+        'nodes=1, mpi=1, omp=2'
+        'nodes=1, mpi=1, omp=4'
+        'nodes=1, mpi=1, omp=8'
+        'nodes=1, mpi=1, omp=12'
+        'nodes=1, mpi=1, omp=24'
+        'nodes=1, mpi=1, omp=36'
+        'nodes=1, mpi=1, omp=48'
+    )
+
+    job_options=(
+        '--exclusive'
+        '--time=00:00:30'
+    )
+    ;;
+CTEARM)
+    commands=(
+        "$binaries_path/bin_gcc/align_benchmark"
+        "$binaries_path/bin_fcc/align_benchmark"
+    )
+
+    parallelism=(
+        'nodes=1, mpi=1, omp=1'
+        'nodes=1, mpi=1, omp=2'
+        'nodes=1, mpi=1, omp=4'
+        'nodes=1, mpi=1, omp=8'
+        'nodes=1, mpi=1, omp=12'
+        'nodes=1, mpi=1, omp=24'
+        'nodes=1, mpi=1, omp=36'
+        'nodes=1, mpi=1, omp=48'
+    )
+    ;;
+*)
+    commands=(
+        "$binaries_path/bin_gcc/align_benchmark"
+    )
+
+    parallelism=(
+        'nodes=1, mpi=1, omp=1'
+        # 'nodes=1, mpi=1, omp=2'
+        # 'nodes=1, mpi=1, omp=4'
+    )
+    ;;
+esac
+
+# Additional arguments to pass to the commands.
+command_opts="-i \"$inputs_path/input.n1M.l100.seq\" -o checksum.file"
+
+#
+# Additional variables.
+#
+
+#
+# This function is executed before launching a job. You can use this function to
+# prepare the stage folder of the job.
+#
+before_run() (
+    job_name="$1"
+)
+
+#
+# This function is executed when a job has finished. You can use this function to
+# perform a sanity check and to output something in the report of a job.
+#
+# echo: The message that you want to output in the report of the job.
+# return: 0 if the run is correct, 1 otherwise.
+#
+after_run() (
+    job_name="$1"
+
+    benchmark_time="$(tac "$job_name.err" | grep -m 1 "Time.Benchmark" | tr -s ' ' | cut -d ' ' -f 3,4)"
+
+    echo "Time.Benchmark: $benchmark_time"
+
+    # Check if the output file is identical to the reference
+    diff --brief checksum.file "$inputs_path/output-reference.file" >/dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        echo "The output file is not identical to the reference file"
+        return 1 # Failure
+    fi
+
+    return 0 # OK
+)
+
+source "$scriptfolder/../../run_wrapper.sh"
