27c6298224bd987e2d7640bd0c807aba21c0aa52
Lorien <lorienlopezv@gmail.com>
2022-05-10 10:12:24 +0200

    Restore removed WFA SVE version

--- benchmarks/wfa/gap_affine/affine_wavefront_extend.c
+++ benchmarks/wfa/gap_affine/affine_wavefront_extend.c
@@ -34,6 +34,51 @@
 #include "gap_affine/affine_wavefront_utils.h"
 #include "utils/string_padded.h"
 
+#ifdef __ARM_FEATURE_SVE
+  #include <arm_sve.h>
+#endif
+
+#if 0
+void print_mask (svbool_t mask) {
+    // Create a "1" vector with inactive lanes to zero
+    svuint32_t res = svdup_u32_z(mask, 1U);
+
+    uint32_t v[16] = {0};
+    svst1_u32(svptrue_b32(), v, res);
+    for (int i=0; i<16; i++) {
+        fprintf(stderr, "%u ", v[i]);
+    }
+    fprintf(stderr, "\n");
+}
+
+void print_svint32 (svint32_t _v) {
+    int32_t v[16] = {0};
+    svst1_s32(svptrue_b32(), v, _v);
+    for (int i=0; i<16; i++) {
+        fprintf(stderr, "%d ", v[i]);
+    }
+    fprintf(stderr, "\n");
+}
+
+void print_svuint32 (svuint32_t _v) {
+    uint32_t v[16] = {0};
+    svst1_u32(svptrue_b32(), v, _v);
+    for (int i=0; i<16; i++) {
+        fprintf(stderr, "%u ", v[i]);
+    }
+    fprintf(stderr, "\n");
+}
+
+void print_svuint32_hex (svuint32_t _v) {
+    uint32_t v[16] = {0};
+    svst1_u32(svptrue_b32(), v, _v);
+    for (int i=0; i<16; i++) {
+        fprintf(stderr, "0x%x ", v[i]);
+    }
+    fprintf(stderr, "\n");
+}
+#endif
+
 /*
  * Reduce wavefront
  */
@@ -107,6 +152,7 @@ void affine_wavefronts_reduce_wavefronts(
     if (dwavefront->lo > dwavefront->hi) dwavefront->null = true;
   }
 }
+
 /*
  * Wavefront offset extension comparing characters
  */
@@ -117,11 +163,118 @@ void affine_wavefronts_extend(
     const char* const text,
     const int text_length,
     const int score) {
+
   // Fetch m-wavefront
   affine_wavefront_t* const mwavefront = affine_wavefronts->mwavefronts[score];
   if (mwavefront==NULL) return;
   // Extend diagonally each wavefront point
   awf_offset_t* const offsets = mwavefront->offsets;
+
+#if defined(__ARM_FEATURE_SVE)
+  #pragma message("affine_wavefront_extend: ARM-SVE version")
+
+  const int k_min = mwavefront->lo;
+  const int k_max = mwavefront->hi;
+  // Extend diagonally each wavefront point
+  int k;
+
+  uint64_t num_elems = svcntw();
+  // fprintf(stderr, "num elements: %zu\n", num_elems);
+  // fprintf(stderr, "text_len: %d, pattern_len=%d\n", text_length, pattern_length);
+
+  // init mask
+  //size_t wf_length = k_max - k_min;
+  svbool_t mask;
+
+  for (k=k_min;k<=k_max;k+=num_elems) {
+    // Get number of elements that will be computed in this iteration
+    //int active_elements = wf_length - k;
+    svint32_t ks = svindex_s32(k, 1);
+    mask = svcmple_s32(svptrue_b32(), ks, svdup_s32(k_max));
+    svbool_t original_mask = mask;
+    // fprintf(stderr, "Ks: "); print_svint32(ks);
+    // fprintf(stderr, "FIRST MASK!! active_elements=%d\n", active_elements);
+
+    svint32_t sv_offsets = svld1(mask, &offsets[k]);
+    // fprintf(stderr, "Offsets: "); print_svint32(sv_offsets);
+
+    bool svtest = svptest_any(svptrue_b32(), mask);
+
+    while (svtest) {
+        // fprintf(stderr, "Initial mask: "); print_mask(mask);
+        // h = o
+        svint32_t h_signed = sv_offsets;
+        // v = o - k
+        svint32_t v_signed = svsub_z(mask, sv_offsets, ks);
+
+        // fprintf(stderr, "vs: "); print_svint32(v_signed);
+        // fprintf(stderr, "hs: "); print_svint32(h_signed);
+        // Cast to unsigned to do the shift
+        svuint32_t v = svreinterpret_u32_s32(v_signed);
+        svuint32_t h = svreinterpret_u32_s32(h_signed);
+        // fprintf(stderr, "v: "); print_svuint32(v);
+        // fprintf(stderr, "h: "); print_svuint32(h);
+
+        svuint32_t bases_pattern = svld1_gather_s32offset_u32(mask, (uint32_t*)pattern, v_signed);
+        bases_pattern = svrevb_u32_z(mask, bases_pattern);
+        svuint32_t bases_text = svld1_gather_s32offset_u32(mask, (uint32_t*)text, h_signed);
+        bases_text = svrevb_u32_z(mask, bases_text);
+        // fprintf(stderr, "Pattern: "); print_svuint32_hex(bases_pattern);
+        // fprintf(stderr, "Text: "); print_svuint32_hex(bases_text);
+        // END TODO
+
+
+        svuint32_t xor_result = sveor_u32_z(mask, bases_pattern, bases_text);
+        // fprintf(stderr, "XOR: "); print_svuint32(xor_result);
+        svuint32_t clz_res = svclz_u32_z(mask, xor_result);
+        // fprintf(stderr, "CLZ: "); print_svuint32(clz_res);
+        svuint32_t Eq = svlsr_u32_z(mask, clz_res, svdup_u32(3U));
+        // fprintf(stderr, "Eq: "); print_svuint32(Eq);
+
+        // Make sure we don't count beyond the sequence
+        svuint32_t remaining_v = svsub_u32_z(mask, svdup_u32(pattern_length), v);
+        svuint32_t remaining_h = svsub_u32_z(mask, svdup_u32(text_length), h);
+        Eq = svmin_u32_z(mask, Eq, remaining_v);
+        Eq = svmin_u32_z(mask, Eq, remaining_h);
+
+        sv_offsets = svadd_s32_m(mask, sv_offsets, svreinterpret_s32(Eq));
+        // fprintf(stderr, "Offsets: "); print_svint32(sv_offsets);
+
+        // Only diagonals that have 4 elements equal (so they have not finished) will continue
+        // fprintf(stderr, "Mask v0: "); print_mask(mask);
+        mask = svcmpgt_n_u32(mask, Eq, 3U);
+
+        // fprintf(stderr, "Mask v1: "); print_mask(mask);
+
+        // v < pattern_length
+        svbool_t mask_v = svcmplt_n_u32(mask, v, (uint32_t)pattern_length);
+        // fprintf(stderr, "Mask v_len: "); print_mask(mask_v);
+        mask = svand_b_z(mask, mask, mask_v);
+        // h < text_length
+        svbool_t mask_h = svcmplt_n_u32(mask, h, (uint32_t)text_length);
+        // fprintf(stderr, "Mask v_len: "); print_mask(mask_h);
+        mask = svand_b_z(mask, mask, mask_h);
+
+        // v++ and h++
+        v = svadd_n_u32_z(mask, v, 4);
+        h = svadd_n_u32_z(mask, h, 4);
+
+        svtest = svptest_any(svptrue_b32(), mask);
+        // // fprintf(stderr, "Mask: "); print_mask(mask); fprintf(stderr, "svtest=%d\n", svtest);
+    }
+    svst1_s32(original_mask, &offsets[k], sv_offsets);
+    // fprintf(stderr, "outside while!\n");
+    //exit(1);
+  }
+  // fprintf(stderr, "\n");
+
+#else
+  #pragma message("affine_wavefront_extend: SCALAR version")
+  // // Fetch m-wavefront
+  // affine_wavefront_t* const mwavefront = affine_wavefronts->mwavefronts[score];
+  // if (mwavefront==NULL) return;
+  // // Extend diagonally each wavefront point
+  // awf_offset_t* const offsets = mwavefront->offsets;
   int k;
   for (k=mwavefront->lo;k<=mwavefront->hi;++k) {
     // Exact extend
@@ -132,7 +285,9 @@ void affine_wavefronts_extend(
       ++(offsets[k]);
     }
   }
+#endif
 }
+
 /*
  * Gap-Affine Wavefront exact extension
  */
