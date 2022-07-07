d14aed553697bdc3567d46cfd0bc9634983722ab
Lorien <lorienlopezv@gmail.com>
2022-07-07 12:10:57 +0200

    DynamoRIO setup path

--- benchmarks/setup_local.sh
+++ benchmarks/setup_local.sh
@@ -7,3 +7,4 @@ export VTUNE_HOME="$(cd "$(dirname "$(which vtune)")/../" && pwd)"
 export VTUNE_INCLUDES="-I${VTUNE_HOME}/include"
 export VTUNE_LDFLAGS="-L${VTUNE_HOME}/lib64 -littnotify -lpthread -ldl"
 
+export DYNAMORIO_INSTALL_PATH="/home/lorien/Repos/dynamorio/build"
--- benchmarks/setup_mn4.sh
+++ benchmarks/setup_mn4.sh
@@ -8,5 +8,7 @@ export VTUNE_HOME="$(cd "$(dirname "$(which amplxe-cl)")/../" && pwd)"
 export VTUNE_INCLUDES="-I${VTUNE_HOME}/include"
 export VTUNE_LDFLAGS="-L${VTUNE_HOME}/lib64 -littnotify -lpthread -ldl"
 
+export DYNAMORIO_INSTALL_PATH="/gpfs/scratch/bsc18/bsc18248/dynamorio/build"
+
 export RAPL_STOPWATCH_INCLUDES="-I/gpfs/scratch/bsc18/bsc18248/rapl_stopwatch/install/include/rapl_stopwatch"
 export RAPL_STOPWATCH_LDFLAGS="-L/gpfs/scratch/bsc18/bsc18248/rapl_stopwatch/install/lib64 -lrapl_stopwatch"
